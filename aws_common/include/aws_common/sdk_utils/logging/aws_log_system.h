3df95efc9933d082958a71a15dd39fd94505e98f
dsiedlak <dennis@sliprobotics.com>
2021-12-23 13:55:39 -0500

    edit to allow other packages to build

--- aws_common/include/aws_common/sdk_utils/logging/aws_log_system.h
+++ aws_common/include/aws_common/sdk_utils/logging/aws_log_system.h
@@ -27,12 +27,12 @@ namespace Aws {
 namespace Utils {
 namespace Logging {
 
-class AWSLogSystem : public Aws::Utils::Logging::LogSystemInterface
-{
+class AWSLogSystem : public Aws::Utils::Logging::LogSystemInterface {
 public:
   explicit AWSLogSystem(Aws::Utils::Logging::LogLevel log_level);
-  AWSLogSystem(AWSLogSystem const &) = delete;              // Do not allow copy constructor
-  AWSLogSystem & operator=(AWSLogSystem const &) = delete;  // Do not allow assignment operator
+  AWSLogSystem(AWSLogSystem const &) = delete; // Do not allow copy constructor
+  AWSLogSystem &
+  operator=(AWSLogSystem const &) = delete; // Do not allow assignment operator
   ~AWSLogSystem() override = default;
 
   // Gets the currently configured log level.
@@ -42,28 +42,29 @@ public:
   void SetLogLevel(Aws::Utils::Logging::LogLevel log_level);
 
   // Log using printf style output. Prefer using LogStream
-  void Log(Aws::Utils::Logging::LogLevel log_level, const char * tag, const char * format,
-                   ...) override;
+  void Log(Aws::Utils::Logging::LogLevel log_level, const char *tag,
+           const char *format, ...) override;
 
   // Logs from a streambuffer.
-  void LogStream(Aws::Utils::Logging::LogLevel log_level, const char * tag,
-                         const Aws::OStringStream & message_stream) override;
+  void LogStream(Aws::Utils::Logging::LogLevel log_level, const char *tag,
+                 const Aws::OStringStream &message_stream) override;
 
 protected:
-  virtual void LogInfo(const char * tag, const std::string & message) = 0;
-  virtual void LogDebug(const char * tag, const std::string & message) = 0;
-  virtual void LogTrace(const char * tag, const std::string & message) = 0;
-  virtual void LogWarn(const char * tag, const std::string & message) = 0;
-  virtual void LogError(const char * tag, const std::string & message) = 0;
-  virtual void LogFatal(const char * tag, const std::string & message) = 0;
+  virtual void LogInfo(const char *tag, const std::string &message) = 0;
+  virtual void LogDebug(const char *tag, const std::string &message) = 0;
+  virtual void LogTrace(const char *tag, const std::string &message) = 0;
+  virtual void LogWarn(const char *tag, const std::string &message) = 0;
+  virtual void LogError(const char *tag, const std::string &message) = 0;
+  virtual void LogFatal(const char *tag, const std::string &message) = 0;
+  virtual void Flush(){};
 
 private:
   std::atomic<Aws::Utils::Logging::LogLevel> configured_log_level_;
 
-  void LogMessage(Aws::Utils::Logging::LogLevel log_level, const char * tag,
-                  const std::string & message);
+  void LogMessage(Aws::Utils::Logging::LogLevel log_level, const char *tag,
+                  const std::string &message);
 };
 
-}  // namespace Logging
-}  // namespace Utils
-}  // namespace Aws
+} // namespace Logging
+} // namespace Utils
+} // namespace Aws
